cda265d5982bc256547d1f2678da232982aa4504
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-07 20:53:00 -0400

    Fixed to read the code from an image

--- puppeteer_script.js
+++ puppeteer_script.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const Tesseract = require('tesseract.js');
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -7,12 +8,19 @@ const puppeteer = require('puppeteer');
   // Navigate to the Chipotle tweet URL
   await page.goto('https://twitter.com/ChipotleTweets/status/1234567890');
 
-  // Extract the code from the tweet's body
-  const codeElement = await page.$('.tweet-body .code');
-  const code = await page.evaluate(element => element.textContent, codeElement);
+  // Extract the image URL from the tweet's body
+  const imageElement = await page.$('.tweet-body img');
+  const imageUrl = await page.evaluate(element => element.src, imageElement);
 
   // Close the browser
   await browser.close();
+
+  // Perform OCR on the image URL using Tesseract.js
+  const { data: { text } } = await Tesseract.recognize(imageUrl);
+
+  // Extract the code from the OCR result
+  const codeMatch = text.match(/(?<=text\s)\S+/);
+  const code = codeMatch ? codeMatch[0] : '';
 
   // Open iMessage
   // Assuming iMessage is already open and the conversation with 888-222 is active
