992f59bdb3eebba0d9b0ac713c295645777af7d1
0xsamuel-eth <sammmastenn@gmail.com>
2023-06-07 20:26:51 -0400

    Created puppeteer script to perform the task

--- puppeteer_script.js
+++ puppeteer_script.js
@@ -1,1 +1,27 @@
 const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate to the Chipotle tweet URL
+  await page.goto('https://twitter.com/ChipotleTweets/status/1234567890');
+
+  // Extract the code from the tweet's body
+  const codeElement = await page.$('.tweet-body .code');
+  const code = await page.evaluate(element => element.textContent, codeElement);
+
+  // Close the browser
+  await browser.close();
+
+  // Open iMessage
+  // Assuming iMessage is already open and the conversation with 888-222 is active
+
+  // Copy the code to the clipboard
+  const { exec } = require('child_process');
+  exec(`echo "${code}" | pbcopy`); // macOS command to copy text to the clipboard
+
+  // Send the code via iMessage
+  exec(`osascript -e 'tell application "Messages" to send "${code}" to buddy "888-222"'`); // macOS command to send iMessage
+
+})();
